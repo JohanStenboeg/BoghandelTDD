3ec9c822c6e1b6a7a9b3638b492cb56ef7a8e033
Johan Stenbøg <Joha6163@edu.easj.dk>
2018-09-27 14:51:31 +0200

    Oprettet bog objektet

---
+++ src/Bog.java
@@ -0,0 +1,10 @@
+public class Bog {
+
+    int antal = 0;
+    String bogTitle = "";
+    String bogForfatter = "";
+    double bogPris = 0.0;
+
+    public Bog(int antal, String bogTitle, String bogForfatter, double bogPris) {
+    }
+}
