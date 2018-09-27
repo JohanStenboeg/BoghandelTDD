18fb121846a3c036edd6c2b0418b9e1a84aeef91
Johan Stenbøg <Joha6163@edu.easj.dk>
2018-09-27 14:50:55 +0200

    4. test lavet til test af bog klassen, samt ændret navn til at matche opgaven

--- src/BogHandelTest.java
+++ src/BogHandelTest.java
@@ -3,18 +3,23 @@ import static org.junit.Assert.*;
 public class BogHandelTest {
     @org.junit.Test
     public void getBogPris(){
-        double pris = 49.95;
+        double pris = 497;
         assertEquals(pris, bog.getPris(), 0.001);
     }
     @org.junit.Test
     public void getBogTitel(){
-        String titel = "Kongen Af Java";
+        String titel = "Java: how to program";
         assertEquals(titel, bog.getTitel(), 0.001);
     }
     @org.junit.Test
     public void getBogTitel(){
-        String forfatter = "Ikke Johan";
+        String forfatter = "Deitel & Deitel";
         assertEquals(forfatter, bog.getAuthor(), 0.001);
+    }
+    @org.junit.Test
+    public void getBogAntal(){
+        int antal = 8;
+        assertEquals(antal, bog.getAntal(), 0.001);
     }
 
 }
