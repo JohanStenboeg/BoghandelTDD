b4d46dd2452e827703f12427b7720da06de97501
Johan Stenbøg <Joha6163@edu.easj.dk>
2018-09-27 15:19:25 +0200

    Tilføjet ny bog i boghandel

--- src/BogHandel.java
+++ src/BogHandel.java
@@ -1,7 +1,7 @@
 public class BogHandel {
     //Opretter første bogobjekt JHTP aka Java, how to program.
-    Bog JHTP = new Bog(8, "Java: how to program", "Deitel & Deitel", 497);
-
+    Bog JHTP = new Bog(8, "Java: how to program", "Deitel & Deitel", 497.00);
+    Bog BJ2 = new Bog(3, "Beginning Java 2", "Ivor Horton", 495.00);
 
 
 
