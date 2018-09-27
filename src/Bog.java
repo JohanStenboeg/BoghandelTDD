e2ff69295eaa1d9018834c06d9423a12aad4b176
Johan Stenbøg <Joha6163@edu.easj.dk>
2018-09-27 15:01:05 +0200

    Oprettet construktorere for bog samt getters

--- src/Bog.java
+++ src/Bog.java
@@ -6,5 +6,42 @@ public class Bog {
     double bogPris = 0.0;
 
     public Bog(int antal, String bogTitle, String bogForfatter, double bogPris) {
+        this.antal = antal;
+        this.bogTitle = bogTitle;
+        this.bogForfatter = bogForfatter;
+        this.bogPris = bogPris;
+    }
+
+    public Bog(double bogPris) {
+        this.bogPris = bogPris;
+    }
+
+    public Bog(String bogTitle) {
+        this.bogTitle = bogTitle;
+    }
+
+    public Bog(int antal) {
+        this.antal = antal;
+    }
+
+    public Bog(String bogTitle, String bogForfatter) {
+        this.bogTitle = bogTitle;
+        this.bogForfatter = bogForfatter;
+    }
+
+    public int getAntal() {
+        return antal;
+    }
+
+    public String getBogTitle() {
+        return bogTitle;
+    }
+
+    public String getBogForfatter() {
+        return bogForfatter;
+    }
+
+    public double getBogPris() {
+        return bogPris;
     }
 }
