d3badbd7defad07c36c681e015b65af9f3e32e67
Johan Stenbøg <Joha6163@edu.easj.dk>
2018-09-27 15:14:15 +0200

    Test på antal af bog java: how to program virker

--- src/BogHandelTest.java
+++ src/BogHandelTest.java
@@ -2,33 +2,32 @@ import static org.junit.Assert.*;
 
 public class BogHandelTest {
     @org.junit.Test
-    public void getBogPris(){
+    public void getBogPris() {
         BogHandel bogHandel = new BogHandel();
 
         double pris = 497;
-        assertEquals(pris, bogHandel.JHTP.getBogPris(),0.001);
+        assertEquals(pris, bogHandel.JHTP.getBogPris(), 0.001);
     }
+
     @org.junit.Test
-    public void getBogTitel(){
+    public void getBogTitel() {
         BogHandel bogHandel = new BogHandel();
         String titel = "Java: how to program";
         assertEquals(titel, bogHandel.JHTP.getBogTitle());
     }
 
     @org.junit.Test
-    public void getBogForfattor(){
+    public void getBogForfattor() {
         BogHandel bogHandel = new BogHandel();
         String forfatter = "Deitel & Deitel";
         assertEquals(forfatter, bogHandel.JHTP.getBogForfatter());
     }
-    /*
+
     @org.junit.Test
-    public void getBogAntal(){
+    public void getBogAntal() {
+        BogHandel bogHandel = new BogHandel();
         int antal = 8;
-        assertEquals(antal, bog.getAntal(), 0.001);
+        assertEquals(antal, bogHandel.JHTP.getAntal());
     }
-    */
-
 }
-
 
